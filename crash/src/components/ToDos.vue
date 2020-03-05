47b8567836f63673f3e16d742e3ef9bd8c85f0bc
Jalia Evans <jalia@nete.com>
2020-03-05 17:31:17 -0500

    working on completing attritbutes and changes to state

---
+++ crash/src/components/ToDos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-for="todo in todos" v-bind:key="todo.id">
+            <ToDoItem v-bind:todo="todo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import ToDoItem from './ToDoItem';
+
+export default {
+    name: 'todos',
+    components: {
+        ToDoItem
+    },
+    props: ["todos"]
+} 
+</script>
+
+<style scoped>
+
+</style>
